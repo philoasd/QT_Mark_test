fa5def40fe89517e338805d915b379f47c337554
phil <1742856967qq.com@gmail.com>
2023-10-12 09:38:29 +0800

    #添加了工具栏和状态栏，将Vision功能放到另一个界面中实现

--- QT_Mark/QT_Mark.cpp
+++ QT_Mark/QT_Mark.cpp
@@ -4,57 +4,120 @@ QT_Mark::QT_Mark(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-
-	//连接信号和槽
-	{
-		//connect(ui.pushButton, &QPushButton::clicked, this, &QT_Mark::Test);
-	}
-
-	m_Camera = new BaslerCamera();
-	Test();
+	InitControl();
+	InitConnect();
 }
 
 QT_Mark::~QT_Mark()
 {
-	delete m_Camera;
+	
 }
 
 void QT_Mark::InitCameraDevices()
 {
+	m_Camera = new BaslerCamera();
+	m_Camera->GetCameraDevices();
+}
 
+void QT_Mark::InitControl()
+{
+	ui.stackedWidget->setCurrentIndex(0);//设置当前显示的页面为第一个页面
+	ui.actionMark->setVisible(false);//隐藏Mark界面的菜单栏
+	ui.toolBar_Vision->setVisible(false);//隐藏图像界面的工具栏
+
+	// 状态栏的操作
+	timer = new QTimer(this);
+	// 添加右对齐的Label到状态栏
+	QLabel* rightAlignedLabel = new QLabel(this);
+	rightAlignedLabel->setAlignment(Qt::AlignRight); // 设置右对齐
+	statusBar()->addPermanentWidget(rightAlignedLabel);
+
+	ui.actionGrabStop->setEnabled(false);// 停止取图按钮不可用
+}
+
+void QT_Mark::InitConnect()
+{
+	// mark界面和图像界面的切换
+	{
+		connect(ui.actionMark, &QAction::triggered, this, [&]() {
+			ui.stackedWidget->setCurrentIndex(0);
+			ui.actionMark->setVisible(false);// 隐藏mark界面的菜单栏
+			ui.actionVision->setVisible(true);// 显示图像界面的菜单栏
+			ui.toolBar_Vision->setVisible(false);// 隐藏图像界面的工具栏
+			ui.toolBar_Shape->setVisible(true);// 显示mark界面的工具栏
+			DisconnectedCamera();
+			ui.actionGrabContinue->setEnabled(true);// 连续取图按钮可用
+			ui.actionGrabOnce->setEnabled(true);// 单次取图按钮可用
+			ui.actionGrabStop->setEnabled(false);// 停止取图按钮不可用
+			});
+		connect(ui.actionVision, &QAction::triggered, this, [&]() {
+			ui.stackedWidget->setCurrentIndex(1);
+			ui.actionVision->setVisible(false);// 隐藏图像界面的菜单栏
+			ui.actionMark->setVisible(true);// 显示mark界面的菜单栏
+			ui.toolBar_Vision->setVisible(true);// 显示图像界面的工具栏
+			ui.toolBar_Shape->setVisible(false);// 隐藏mark界面的工具栏
+			InitCameraDevices();
+			ConnectedCamera();
+			});
+	}
+
+	// 状态栏的操作
+	{
+		connect(timer, &QTimer::timeout, this, [&]() {
+			ui.statusBar->findChild<QLabel*>()->setText(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd"));// 设置状态栏的时间
+			});
+		timer->start(1000); }
+
+	// 图像界面的操作
+	{
+		// 连续取图
+		connect(ui.actionGrabContinue, &QAction::triggered, this, [&]() {
+			m_Camera->StartCapture(1);
+			ui.actionGrabContinue->setEnabled(false);// 连续取图按钮不可用
+			ui.actionGrabOnce->setEnabled(false);// 单次取图按钮不可用
+			ui.actionGrabStop->setEnabled(true);// 停止取图按钮可用
+			});
+		// 单次取图
+		connect(ui.actionGrabOnce, &QAction::triggered, this, [&]() {
+			m_Camera->StartCapture(0);
+			});
+		// 停止取图
+		connect(ui.actionGrabStop, &QAction::triggered, this, [&]() {
+			m_Camera->StopCapture();
+			ui.actionGrabContinue->setEnabled(true);// 连续取图按钮可用
+			ui.actionGrabOnce->setEnabled(true);// 单次取图按钮可用
+			ui.actionGrabStop->setEnabled(false);// 停止取图按钮不可用
+			});
+	}
 }
 
 void QT_Mark::ShowImage(const CGrabResultPtr& ptrGrabResult)
 {
 	if (ptrGrabResult->GrabSucceeded())
 	{
-		QImage img = ImageConvert::ConvertToQImage(ptrGrabResult);
+		QImage img = ImageConvert::ConvertToQImage(ptrGrabResult);// 将Basler图像原始数据转换为QImage
 		ui.label_ShowImage->setPixmap(QPixmap::fromImage(img).scaled(ui.label_ShowImage->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
-		img.save("test.bmp");
 	}
 }
 
-void QT_Mark::Test()
+void QT_Mark::ConnectedCamera()
 {
-	auto devicesInfo = m_Camera->GetCameraDevices();
 	m_Camera->ConnectedCamera(0);
 	callback = [&](const CGrabResultPtr& ptrGrabResult) {
 		ShowImage(ptrGrabResult);
 		};
 	m_ImageEventHandler = new ImageEventHandler(callback);
 	m_Camera->RegisterImageEvent(m_ImageEventHandler);
-	m_Camera->StartCapture(0);
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-	m_Camera->StopCapture();
+}
+
+void QT_Mark::DisconnectedCamera()
+{
+	m_Camera->DisConnectedCamera();
 	if (m_ImageEventHandler != nullptr) {
 		//callback = nullptr;
 		m_Camera->DeregisterImageEvent(m_ImageEventHandler);
-		if (m_ImageEventHandler != nullptr)
-
-			m_ImageEventHandler = nullptr;
+		m_ImageEventHandler = nullptr;
 		delete m_ImageEventHandler;
 	}
-
-	m_Camera->DisConnectedCamera();
-
+	delete m_Camera;
 }
--- QT_Mark/QT_Mark.h
+++ QT_Mark/QT_Mark.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <QtWidgets/QMainWindow>
 #include "ui_QT_Mark.h"
@@ -6,6 +6,8 @@
 #include "ImageConvert.h"
 #include <thread>
 #include <chrono>
+#include <qtimer.h>
+#include <qdatetime.h>
 
 class QT_Mark : public QMainWindow
 {
@@ -15,14 +17,43 @@ public:
 	QT_Mark(QWidget* parent = nullptr);
 	~QT_Mark();
 private:
+	/// <summary>
+	/// 获取相机设备
+	/// </summary>
 	void InitCameraDevices();
+
+	/// <summary>
+	/// 初始化控件，一般是对控件的一些显示或者属性进行设置
+	/// </summary>
+	void InitControl();
+
+	/// <summary>
+	/// 初始化连接，一般是对信号和槽进行连接
+	/// </summary>
+	void InitConnect();
+
+	/// <summary>
+	/// 显示图像
+	/// </summary>
+	/// <param name="ptrGrabResult">Basler图像原始数据</param>
 	void ShowImage(const CGrabResultPtr& ptrGrabResult);
 
+	/// <summary>
+	/// 连接相机
+	/// </summary>
+	void ConnectedCamera();
+
+	/// <summary>
+	/// 断开相机连接
+	/// </summary>
+	void DisconnectedCamera();
+
 private slots:
-	void Test();
+
 private:
 	Ui::QT_MarkClass ui;
-	BaslerCamera* m_Camera = nullptr;
-	ImageEventHandler::ImageCallback callback;
-	ImageEventHandler* m_ImageEventHandler = nullptr;
+	BaslerCamera* m_Camera = nullptr; // 相机对象
+	ImageEventHandler::ImageCallback callback; // 图像回调函数
+	ImageEventHandler* m_ImageEventHandler = nullptr; // 图像回调对象
+	QTimer* timer; // 系统时间
 };
