4940d7050bec8585794cf63de64c2f8d6acbc259
phil <1742856967qq.com@gmail.com>
2023-10-23 23:08:22 +0800

    #

--- ImageLibrary/HalconLibrary.cpp
+++ ImageLibrary/HalconLibrary.cpp
@@ -1,2 +1,16 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "HalconLibrary.h"
+
+void HalconLibrary::ShowImage(const HObject& image, Hlong winID, int width, int height)
+{
+	// 如果窗口句柄为空，则创建窗口
+	if (windowHandle == NULL)
+	{
+		OpenWindow(0, 0, width, height, winID, "visible", "", &windowHandle);
+		HDevWindowStack::Push(windowHandle);
+	}
+	if (HDevWindowStack::IsOpen()) {
+		DispObj(image, HDevWindowStack::GetActive());
+	}
+	DispObj(image, windowHandle);
+}
--- ImageLibrary/HalconLibrary.h
+++ ImageLibrary/HalconLibrary.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <halconcpp/HalconCpp.h>
 
@@ -6,5 +6,15 @@ using namespace HalconCpp;
 
 class __declspec(dllexport) HalconLibrary
 {
+public:
+	/// <summary>
+	/// 显示图像
+	/// </summary>
+	/// <param name="image">图像</param>
+	/// <param name="winID">窗口控件</param>
+	void ShowImage(const HObject& image, Hlong winID, int width, int height);
+
+private:
+	HTuple windowHandle;// 显示窗口句柄
 };
 
